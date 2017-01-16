8838247c92fb73f39d90cb5ff06e763dad44f0a9
K9326 <K9326@student.labranet.jamk.fi>
2017-01-16 13:55:23 +0200

    Kiuas lisätty

---
+++ OOdemot/Kiuas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    public class Kiuas
+    {
+        public bool OnkoPäällä { get; set; }
+        public float Lämpötila { get; set; }
+        // kosteus voi olla välillä 0-100
+        float kosteus;
+        public float Kosteus {
+            get {return kosteus;}
+            
+            set
+            {
+                kosteus = value;
+                if (kosteus < 0 || kosteus > 100)
+                    kosteus = 0;
+            }
+        }
+
+
+    }
+}
---
+++ OOdemot/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JAMK.IT; // helpottaa meidän luokkien löytämistä
+
+namespace OOdemot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestaaKiuas();
+        }
+        // tehtävän 1 Kiuas-luokan testaus
+        static void TestaaKiuas()
+        {
+            // luodaan kiuas olio
+            Kiuas kiuas = new JAMK.IT.Kiuas();
+            //pistetääs kiuas lämpenemään ja asetetaan lämpöä & kosteutta
+            kiuas.OnkoPäällä = true;
+            kiuas.Lämpötila = 90;
+            kiuas.Kosteus = 50;
+            // näytetään konsolilla
+            Console.WriteLine("Kiuas on päällä {0}", kiuas.OnkoPäällä);
+            Console.WriteLine("Kiukaan lämpötila on {0}", kiuas.Lämpötila);
+            Console.WriteLine("Kiukaan kosteus {0}", kiuas.Kosteus);
+            // jos yli rajojen, mitä tapahtuu?
+            kiuas.Kosteus = 103;
+            Console.WriteLine("Kiukaan kosteus {0}", kiuas.Kosteus);
+
+        }
+    }
+}
